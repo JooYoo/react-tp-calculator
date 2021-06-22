d00631541500982106a2468048815a749b35e634
JooYoo <jooyoo@outlook.com>
2021-06-22 05:31:52 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Calculator from "./components/Calculator";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div>React TinyProjects 2/9: Calculator</div>
+      <Calculator />
+    </div>
+  );
+}
---
+++ src/components/Calculator.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Calculator = () => {
+  return (
+    <div>
+      <h4>calc cp</h4>
+    </div>
+  );
+};
+
+export default Calculator;
