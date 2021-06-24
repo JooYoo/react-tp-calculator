480c0d3297203e667d23458fbd79f8c249b8deb9
JooYoo <jooyoo@outlook.com>
2021-06-24 08:53:53 +0200

    feat: nums, dot

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -23,6 +23,26 @@ const Calculator = () => {
     setCurrent(newCurr);
   };
 
+  const append = (num) => {
+    if (current === "0" && num === ".") {
+      // 0.x
+      setCurrent(`0${num}`);
+    } else if (current === "0") {
+      // clear default 0 at beginning
+      setCurrent(num);
+    } else {
+      // update number
+      setCurrent(`${current}${num}`);
+    }
+  };
+
+  const dot = () => {
+    // if dot not exist then add
+    if (current.indexOf(".") === -1) {
+      append(".");
+    }
+  };
+
   return (
     <div>
       <div className="calculator">
@@ -37,20 +57,42 @@ const Calculator = () => {
           %
         </div>
         <div className="btn operator">÷</div>
-        <div className="btn">7</div>
-        <div className="btn">8</div>
-        <div className="btn">9</div>
+        <div className="btn" onClick={() => append("7")}>
+          7
+        </div>
+        <div className="btn" onClick={() => append("8")}>
+          8
+        </div>
+        <div className="btn" onClick={() => append("9")}>
+          9
+        </div>
         <div className="btn operator">⨉</div>
-        <div className="btn">4</div>
-        <div className="btn">5</div>
-        <div className="btn">6</div>
+        <div className="btn" onClick={() => append("4")}>
+          4
+        </div>
+        <div className="btn" onClick={() => append("5")}>
+          5
+        </div>
+        <div className="btn" onClick={() => append("6")}>
+          6
+        </div>
         <div className="btn operator">−</div>
-        <div className="btn">1</div>
-        <div className="btn">2</div>
-        <div className="btn">3</div>
+        <div className="btn" onClick={() => append("1")}>
+          1
+        </div>
+        <div className="btn" onClick={() => append("2")}>
+          2
+        </div>
+        <div className="btn" onClick={() => append("3")}>
+          3
+        </div>
         <div className="btn operator">+</div>
-        <div className="btn zero">0</div>
-        <div className="btn">.</div>
+        <div className="btn zero" onClick={() => append("0")}>
+          0
+        </div>
+        <div className="btn" onClick={dot}>
+          .
+        </div>
         <div className="btn operator">=</div>
       </div>
     </div>
